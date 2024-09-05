d01d21636562b1233ab62d7ee716b791ffe2eafa
Luis Mac <luismacod5@gmail.com>
2024-09-05 14:27:00 -0600

    Add initial project files and configuration

---
+++ scholar-api-integation/src/main/java/com/google/scholarapi/scholar_api_integation/ScholarApiIntegationApplication.java
@@ -0,0 +1,13 @@
+package com.google.scholarapi.scholar_api_integation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScholarApiIntegationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScholarApiIntegationApplication.class, args);
+	}
+
+}
---
+++ scholar-api-integation/src/test/java/com/google/scholarapi/scholar_api_integation/ScholarApiIntegationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.google.scholarapi.scholar_api_integation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScholarApiIntegationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
